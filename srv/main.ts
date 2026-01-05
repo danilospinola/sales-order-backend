2d601258ac91805dbd0fbdb6ef19f0764d441ff7
danilospinola <danspinola10@gmail.com>
2026-01-05 10:19:29 -0300

    feat: add after read customers logic

---
+++ srv/main.ts
@@ -0,0 +1,12 @@
+import {Service} from '@sap/cds';
+import {Customers} from '@cds-models/sales';
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers) => {
+        results.forEach(customer => {
+            if (customer.email?.includes('@')) {
+                customer.email = `${customer.email}@gmail.com`;
+            }
+        })
+    });
+}
