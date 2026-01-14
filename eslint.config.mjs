87590eb472756cc7cb90febd474053e8263e5edd
danilospinola <danspinola10@gmail.com>
2026-01-14 09:32:17 -0300

    feat: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,67 @@
-import cds from '@sap/cds/eslint.config.mjs'
-export default [ ...cds.recommended ]
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import { rules } from "eslint-plugin-react";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['./gen/*.{js,ts}'],
+    files: ['**/*.{mjs,js,ts}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error', {
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^ignore',
+          ignoresRestSiblings: true
+        }
+      ],
+      'eol-last': 'error',
+      'indent': [
+        'error',
+        4,
+        {
+          SwitchCase: 1
+        }
+      ],
+      'max-len': [
+        'error',
+        120
+      ],
+      'max-lines-per-function': [
+        'error',
+        30
+      ],
+      'object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      quotes: [
+        'error',
+        'single'
+      ],
+      'quotes-props': [
+        'error',
+        'as-needed'
+      ],
+      semi: [
+        'error',
+        'always'
+      ],
+      'sort-improts': [
+        'error',
+        {
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true
+        }
+      ]
+    }
+  }
+]
