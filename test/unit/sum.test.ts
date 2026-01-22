a049d249046e21714f6727f98cb1eac2e4db2a1a
danilospinola <danspinola10@gmail.com>
2026-01-22 12:54:13 -0300

    chore: add vitest and usages

--- test/unit/sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect} from "vitest";
-
-describe("sum module", () => {
-    it("should sum two numbers correctly", () => {
-        const sum = (a: number, b: number): number => a + b;
-        expect(sum(2, 3)).toBe(5);
-    });
-});
